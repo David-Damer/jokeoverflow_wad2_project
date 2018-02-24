2054a220a5f3e4a10b7a3aafbed3e28c4ef1944a
David Damer <2266980D@student.gla.ac.uk>
2018-02-24 12:24:38 +0000

    fix to admin.py

--- jokeoverflow/admin.py
+++ jokeoverflow/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from jokeoverflow.models import Category, Video
 from jokeoverflow.models import UserProfile, Joke, Comment
 # Register your models here.
-admin.site.register(Category, Video)
-admin.site.register(UserProfile, Joke)
+admin.site.register(Category)
+admin.site.register(UserProfile)
 admin.site.register(Comment)
+admin.site.register(Video)
+admin.site.register(Joke)
+
