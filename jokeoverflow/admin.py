3ec58c3770dbe95d49c5b24a70ddb6990d2da37a
David Damer <2266980D@student.gla.ac.uk>
2018-02-24 08:18:45 +0000

    registered models

--- jokeoverflow/admin.py
+++ jokeoverflow/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from jokeoverflow.models import Category, Video, UserProfile, Joke, Comment
 
 # Register your models here.
+admin.site.register(Category, Video, UserProfile, Joke, Comment)
