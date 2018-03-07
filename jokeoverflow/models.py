c9b5615690529a041f2cd1a8797c23880566852f
David Damer <2266980D@student.gla.ac.uk>
2018-03-07 16:15:02 +0000

    added slug field to joke

--- jokeoverflow/models.py
+++ jokeoverflow/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
-from datetime import datetime
-from django.utils.timezone import now
 
 
 # Create your models here.
@@ -65,6 +63,11 @@ class Joke(models.Model):
     category = models.ForeignKey(Category)
     added_by = models.ForeignKey(User)
     rating = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Joke, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
