c670f64fcaa635d7fae7171273a41eb2010c0ec1
David Damer <2266980D@student.gla.ac.uk>
2018-03-07 16:56:49 +0000

    updated admin with slugs

--- jokeoverflow/admin.py
+++ jokeoverflow/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from jokeoverflow.models import Category, Video
 from jokeoverflow.models import UserProfile, Joke, Comment
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+
+class JokeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(UserProfile)
 admin.site.register(Comment)
 admin.site.register(Video)
-admin.site.register(Joke)
+admin.site.register(Joke, JokeAdmin)
 
