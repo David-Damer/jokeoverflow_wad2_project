ee3c270e7bb6acfb2e89d91e85682240a67cb390
David-Damer <2266980d@student.gla.ac.uk>
2018-03-08 15:56:07 +0000

    fixed typo in ajax file

--- static/js/jokeoverflow-ajax.js
+++ static/js/jokeoverflow-ajax.js
@@ -1,25 +1,25 @@
-$('#likes').click(function(){
+$('#likes').click(function () {
     var catid;
     catid = $(this).attr("data-catid");
-    $.get('/rango/like/', {category_id: catid}, function(data){
+    $.get('/rango/like/', {category_id: catid}, function (data) {
         $('#like_count').html(data);
         $('#likes').hide();
     });
 });
-$('#suggestion').keyup( function(){
+$('#suggestion').keyup(function () {
     var query;
     query = $(this).val();
-    $.get('/rango/suggest/', {suggestion: query}, function(data){
+    $.get('/rango/suggest/', {suggestion: query}, function (data) {
         $('#cats').html(data);
     });
 });
-   $('.rango-add').click(function(){
-	    var catid = $(this).attr("data-catid");
-        var url = $(this).attr("data-url");
-        var title = $(this).attr("data-title");
-        var me = $(this)
-	    $.get('/rango/add/', {category_id: catid, url: url, title: title}, function(data){
-	                   $('#pages').html(data);
-	                   me.hide();
-	               });
-	    });
+$('.rango-add').click(function () {
+    var catid = $(this).attr("data-catid");
+    var url = $(this).attr("data-url");
+    var title = $(this).attr("data-title");
+    var me = $(this);
+    $.get('/rango/add/', {category_id: catid, url: url, title: title}, function (data) {
+        $('#pages').html(data);
+        me.hide();
+    });
+});
