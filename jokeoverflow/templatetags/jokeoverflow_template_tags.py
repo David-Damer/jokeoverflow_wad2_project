3bb0ead5198facd15a8c7b4c6c33e4320145fd6d
Sam Cook <sam.d.cook.98@gmail.com>
2018-03-24 21:44:33 +0000

    Adding Flag Button

--- jokeoverflow/templatetags/jokeoverflow_template_tags.py
+++ jokeoverflow/templatetags/jokeoverflow_template_tags.py
@@ -4,9 +4,9 @@ from jokeoverflow.models import Joke,UserProfile
 register = template.Library()
 
 @register.inclusion_tag('jokeoverflow/jokes.html')
-def get_joke_list(joke=None):
+def get_joke_pop(joke=None, user=None):
     return {'jokes':Joke.objects.all(),
             'act_joke': joke,
             'users': UserProfile.objects.all(),
-            
+            'act_user': user,
             }
