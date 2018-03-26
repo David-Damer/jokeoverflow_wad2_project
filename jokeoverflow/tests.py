d0059bc38701ace2b6b6e218790ce773afb10b7e
Joe Kadi <joekadi@Joes-MacBook-Pro.local>
2018-03-26 18:46:12 +0100

    unit tests

--- jokeoverflow/tests.py
+++ jokeoverflow/tests.py
@@ -1,57 +1,81 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
-from jokeoverflow.models import Category
+from django.contrib.staticfiles import finders
+from django.utils import timezone
+from django.contrib.auth.models import User
+from jokeoverflow.forms import *
+from jokeoverflow.models import *
 
 
+class GenericTests(TestCase):
 
-class CategoryMethodTests(TestCase):
+    def test_working(self):
 
-    # helper method
-    
-    def add_cat(name, views, likes):
-        
-        # adds category to dictionary
+        # test to check if tests are working via simple addition
 
-        c = Category.objects.get_or_create(name=name)[0]
-        c.views = views
-        c.likes = likes
-        c.save()
-        return c
+        self.assertEqual(1 + 1, 2)
 
-    # Create your tests here.
+    def test_static_files(self):
 
-    def test_slug_line_creation(self):
+        # test if static media is used correctly. expepcted output = not NONE
 
-        # tests to check that when we add a category an appropiate slug line is created,
-        # i.e 'Category String' --> category-string.
+        result = finders.find('images/logo.png')
+        self.assertIsNotNone(result)
 
-        cat = cat('Random Category String')
-        cat.save()
-        self.assertEqual(cat.slug, 'random-category-string')
+    def test_home_response(self):
 
-    def test_home_view_with_no_categories(self):
-
-        # test to see if homepage view loads with no categories, appropiate message displayed
+        # tests where the homepage url responds
 
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "There are no categories present.")
-        self.assertQuerysetEqual(response.context['categories'], [])
-
-    def test_home_view_with_categories(self):
-
-        # check to make sure that the homepage has categories displayed
         
-        add_cat('test',1,1)
-        add_cat('temp',1,1)
-        add_cat('tmp',1,1)
-        add_cat('tmp test temp',1,1)
+    def test_home_template(self):
 
-        response = self.client.get(reverse('index'))
-        self.assertEqual(reponse.status_code, 200)
-        self.assertContains(response, "tmp test temp")
+        # test to check if homepage using correct template
 
-        num_cats = len(response.context['catergories'])
-        self.assertEqual(num_cats, 4)
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'jokeoverflow/base2.html')
+
+    def test_population_script(self):
+
+        # tests to check if population script runs
         
+        try:
+            from population_script import populate_jokeoverflow
+            populate_jokeoverflow()
+        except ImportError:
+            print('population_script does not exist')
+        except NameError:
+            print('The function populate() does not exist or is not correct')
+        except:
+            print('Something went wrong in the populate() function')
 
+    def test_valid_user(self):
+
+        # tests userform with valid data
+        
+        form = UserForm(data={'username': "testuser", 'email': "test@test.com", 'password': "123abc123"})
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_user(self):
+
+        # tests userform with invalid data
+        
+        form = UserForm(data={'username': "testuser", 'email': "testtest.com", 'password': "n/a"})
+        self.assertFalse(form.is_valid())
+
+    def test_valid_category(self):
+
+        # tests if category model works
+        
+        category = Category.objects.create(title="test category", restricted=False, no_of_jokes=1, slug="test_category")
+        self.assertTrue(isinstance(category, Category))
+
+    def test_valid_joke(self):
+
+        # test if joke creation with category and user
+        
+        user = User.objects.create(username="testuser", password="testpass")
+        category = Category.objects.create(title="test category", restricted=False, no_of_jokes=1, slug="test_category")
+        joke = Joke.objects.create(title="title", joke_text="joketext", date_added=timezone.now(), upvotes=1, downvotes=1, flagged=False, category=category, added_by=user, rating=1, slug="title") 
+        self.assertTrue(isinstance(joke, Joke))
